d1336d685db6664491de5124cf2d1e18f5c0bddb
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2017-04-21 12:14:48 -0700

    update environment variable for mailer

--- src/api/mailer.js
+++ src/api/mailer.js
@@ -1,21 +1,24 @@
 import nodemailer from 'nodemailer';
-import {gmail} from '../config/local';
+//import {gmail} from '../config/local';
 
 export default (req, res) => {
     // create reusable transporter object using the default SMTP transport
+    let email = process.env.EMAIL;
+    let password = process.env.PASSWORD;
+
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: process.env.EMAIL || gmail.email ,
-            pass: process.env.PASSWORD || gmail.password
+            user: email,
+            pass: password
         }
     });
 
 // setup email data with unicode symbols
     let {subject, message} = req.body;
     let mailOptions = {
-        from: `"Website/Business Name" <${gmail.email}>`, // gmail does not allow a different from address but can change the name
-        to: gmail.email, // contact form therefore email to yourself
+        from: `"Website/Business Name" <${email}>`, // gmail does not allow a different from address but can change the name
+        to: email, // contact form therefore email to yourself
         subject, // Subject line
         text: message, // plain text body
         html: `<b>${message}</b>` // html body
